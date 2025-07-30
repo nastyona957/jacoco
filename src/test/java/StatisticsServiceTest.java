10151a092bd0346012774fa06beef1a292a5051b
anastasia.test <nastyona957@yandex.ru>
2025-07-30 12:21:04 +0300

    Tests added

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -14,4 +14,40 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void findMax_whenAllElementsAreSame() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 5, 5, 5, 5};
+        long expected = 5;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMax_IfFirstNotMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {3, 7, 9, 4, 5, 1, 9, 6, 15, 2, 1};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMax_whenMaxIsLastElement() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {2, 8, 4, 5, 10};
+        long expected = 10;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
